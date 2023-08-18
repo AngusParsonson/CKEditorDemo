207d21929e21c2d346e540f9c46fb6d5d8f3a768
angus parsonson <angus@anguss-MacBook-Pro.local>
2023-08-18 16:33:36 -0700

    fin

--- test-ckeditor/src/App.tsx
+++ test-ckeditor/src/App.tsx
@@ -9,40 +9,38 @@ const editorConfiguration = {
 };
 
 class App extends Component {
-  editor: DecoupledEditor | null = null;
+  editorRef: DecoupledEditor | null = null;
 
   render() {
     return (
       <div className="App">
-        <h2>Using CKEditor&nbsp;5 from online builder in React</h2>
-        <div className="document-editor">
-          <CKEditor
-            editor={Editor}
-            config={editorConfiguration}
-            data="<p>Hello from CKEditor&nbsp;5!</p>"
-            onReady={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
-              editor?.ui
-                ?.getEditableElement()
-                ?.parentElement?.insertBefore(
-                  editor?.ui?.view?.toolbar?.element as Node,
-                  editor.ui.getEditableElement() as Node
-                );
+        <h2>CKEditor 5 using a custom build - DecoupledEditor</h2>
 
-              this.editor = editor;
-            }}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              console.log({ event, editor, data });
-            }}
-            onBlur={(event, editor) => {
-              console.log("Blur.", editor);
-            }}
-            onFocus={(event, editor) => {
-              console.log("Focus.", editor);
-            }}
-          />
+        <div className="document-editor">
+          <div className="document-editor__toolbar"></div>
+          <div className="document-editor__editable-container">
+            <CKEditor
+              config={editorConfiguration}
+              onReady={(editor) => {
+                console.log("Editor is ready to use!", editor);
+
+                // Add these two lines to properly position the toolbar
+                const toolbarContainer = document.querySelector(
+                  ".document-editor__toolbar"
+                );
+                if (this.editorRef) {
+                  toolbarContainer?.appendChild(
+                    editor.ui.view.toolbar.element as Node
+                  );
+                }
+                this.editorRef = editor;
+                editor = editor;
+              }}
+              onChange={(event, editor) => console.log({ event, editor })}
+              editor={Editor}
+              data="<p>Hello from CKEditor 5's DecoupledEditor!</p>"
+            />
+          </div>
         </div>
       </div>
     );
