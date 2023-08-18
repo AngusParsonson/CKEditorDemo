6b9ac6b616cdba0cffdeb9c145769e41bc01f9f2
angus parsonson <angus@anguss-MacBook-Pro.local>
2023-08-18 16:21:37 -0700

    fin

--- test-ckeditor/src/App.tsx
+++ test-ckeditor/src/App.tsx
@@ -1,48 +1,49 @@
 import { Component } from "react";
-// import Editor from "ckeditor5-custom-build/build/ckeditor";
 import Editor from "ckeditor5-custom-build";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+import { DecoupledEditor } from "@ckeditor/ckeditor5-editor-decoupled";
 
 const editorConfiguration = {
-  toolbar: ["bold", "italic"],
   licenseKey:
     "SGJGQVBtYklMNkp2Zi81WW91Z3Nia3NMeXQwdHNoSjNXQ29ya3BLQXIxek03dE5zSHVQc0NaazRRMzNKLU1qQXlNekE1TURjPQ==",
 };
 
 class App extends Component {
-  editor = null;
+  editor: DecoupledEditor | null = null;
 
   render() {
     return (
       <div className="App">
         <h2>Using CKEditor&nbsp;5 from online builder in React</h2>
-        <CKEditor
-          editor={Editor}
-          config={editorConfiguration}
-          data="<p>Hello from CKEditor&nbsp;5!</p>"
-          onReady={(editor) => {
-            // You can store the "editor" and use when it is needed.
-            console.log("Editor is ready to use!", editor);
-            editor.ui
-              .getEditableElement()
-              .parentElement.insertBefore(
-                editor.ui.view.toolbar.element,
-                editor.ui.getEditableElement()
-              );
+        <div className="document-editor">
+          <CKEditor
+            editor={Editor}
+            config={editorConfiguration}
+            data="<p>Hello from CKEditor&nbsp;5!</p>"
+            onReady={(editor) => {
+              // You can store the "editor" and use when it is needed.
+              console.log("Editor is ready to use!", editor);
+              editor?.ui
+                ?.getEditableElement()
+                ?.parentElement?.insertBefore(
+                  editor?.ui?.view?.toolbar?.element as Node,
+                  editor.ui.getEditableElement() as Node
+                );
 
-            this.editor = editor;
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-          }}
-          onBlur={(event, editor) => {
-            console.log("Blur.", editor);
-          }}
-          onFocus={(event, editor) => {
-            console.log("Focus.", editor);
-          }}
-        />
+              this.editor = editor;
+            }}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              console.log({ event, editor, data });
+            }}
+            onBlur={(event, editor) => {
+              console.log("Blur.", editor);
+            }}
+            onFocus={(event, editor) => {
+              console.log("Focus.", editor);
+            }}
+          />
+        </div>
       </div>
     );
   }
